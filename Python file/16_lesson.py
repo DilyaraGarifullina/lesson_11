282ad844cb45de3950119ca948fc71f9cbd36eb6
Dilyara <dilyaragar1@mail.ru>
2023-05-04 11:09:18 +0300

    lesson_16

--- Python file/16_lesson.py
+++ Python file/16_lesson.py
@@ -6,9 +6,45 @@
 #и абстрактный метод:
 #say - издать звук.
 #Создайте два класса потомка - Cat и Dog, в которых будет переопределен метод say: для класса Cat - Meow!, для Dog - Woof!
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    def __init__(self, color, name, age):
+        self.color = color
+        self.name = name
+        self.age = age
+        @abstractmethod
+        def say(self):
+            pass
+class Cat(Animal):
+    def say(self):
+        return 'Meow!'
+
+class Dog(Animal):
+    def say(self):
+        return 'Woof!'
+
+cat = Cat('orange', 'Murka', 1)
+dog = Dog('black', 'Wolff', 6)
+
+
+print(f'{cat.name} ({cat.color}, {cat.age} year(s) old) says {cat.say()}')
+print(f'{dog.name} ({dog.color}, {dog.age} year(s) old) says {dog.say()}')
 
 #Задание 2
 #Создайте базовый класса Father, у которого есть два атрибута: name (имя) и surname (фамилия); и дочерний класс Child, у которого будут унаследованы те же атрибуты и дополнительно атрибут patronim (отчество). Создайте один экземпляр класса Child.
+class Father:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+class Child(Father):
+    def __init__(self, name, surname, patronim):
+        super().__init__(name, surname)
+        self.patronim = patronim
+
+child = Child("Диляра", " Гарифуллина","Ильясовна")
+print(child.name, child.surname, child.patronim)
 
 #Задание 3
 #А) Реализовать класс Stationery (канцелярия):
@@ -23,7 +59,51 @@
 #Добавьте в класс Stationary метод класса set_color, который присваивает атрибут класса Stationary color;
 #Вызовете метод set_color и установите color='yellow';
 #Вызовете атрибуты color у классов Pen, Pencil, Handle. Что вы наблюдаете?
+from abc import ABC, abstractmethod
 
+
+class Stationery(ABC):
+    def __init__(self, title):
+        self.title = title
+
+    @abstractmethod
+    def set_color(self, color):
+        self.color = color
+
+    @abstractmethod
+    def draw(self):
+        pass
+
+
+class Pen(Stationery):
+    color = 'blue'
+
+    def draw(self):
+        print("Ручка пишет")
+
+
+class Pencil(Stationery):
+    def draw(self):
+        print("Карандаш чертит")
+
+
+class Handle(Stationery):
+    def draw(self):
+        print("Маркер рисует")
+
+
+pen = Pen("Ручка")
+pencil = Pencil("Карандаш")
+handle = Handle("Маркер")
+
+pen.draw()
+pencil.draw()
+handle.draw()
+
+Stationery.self_color('yellow')
+print(Pen.color)
+print(Pencil.color)
+print(Handle.color)
 #Задание 4
 #Создайте базовый класс User с атрибутами login, password и методом:
 #view - выводит сообщение "Я - User. Могу просматривать содержимое"
@@ -31,6 +111,33 @@
 #view - выводит сообщение "Я - Moderator. Могу просматривать содержимое"
 #redact - выводит сообщение "Я - Moderator. Могу редактировать содержимое"
 
+class User:
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
+
+    def view(self):
+        print("Я - User. Могу просматривать содержимое")
+
+
+class Moderator(User):
+    def __init__(self, login, password, group_id):
+        super().__init__(login, password)
+        self.group_id = group_id
+
+    def view(self):
+        print("Я - Moderator. Могу просматривать содержимое")
+
+    def redact(self):
+        print("Я - Moderator. Могу редактировать содержимое")
+
+
+user = User("user_login", "user_password")
+moderator = Moderator("8541851", "54154", "911")
+
+user.view()
+moderator.view()
+moderator.redact()
 #Задание 5
 #Создайте класс Clock со статичным методом Say_time, который будет выводить на экран текущее время.
 
@@ -53,6 +160,11 @@
 #print(f'{rez.tm_hour}:{rez.tm_min}:{rez.tm_sec}') # 10:29:45
 #1668680985.4531398
 #10:29:45
+from time import time, localtime
+class Clock:
+    @staticmethod
+    def Say_time():
+        time = localtime()
+        print(f'Текущее время: {time.tm_hour}:{time.tm_min}:{time.tm_sec}')
 
-#Задание 6 * (необязательное)
-#Давайте создадим свой класс Time по аналогии с Date_time выше. Реализуйте в нем классовый метод from_string(), который будет принимать в себя строку('00:00:00') и будет преобразовывать его в объект Time.
+Clock.Say_time()
